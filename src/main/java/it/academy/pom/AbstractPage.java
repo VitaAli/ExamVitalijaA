f6210872bcbc7d257cccd7aeab034c6630e60940
Vitalija Ališauskienė <vitalija.alisauskiene@stud.vtmc.lt>
2023-06-28 14:11:50 +0300

    first classes were added

--- src/main/java/it/academy/pom/AbstractPage.java
+++ src/main/java/it/academy/pom/AbstractPage.java
@@ -1,2 +1,14 @@
-package it.academy.pom;public class AbstractPage {
+package it.academy.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class AbstractPage {
+
+    protected final WebDriver driver;
+
+    public AbstractPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 }
